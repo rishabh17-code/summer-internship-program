8b36d1ad887087ffcfbeba9247ad1759ceffc7a3
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-07 16:12:00 +0530

    some extra topic in es6

--- ECMA6/advance.js
+++ ECMA6/advance.js
@@ -27,3 +27,36 @@
 // let newarray2 = arr.splice(0,3,100,200)
 // console.log(newarray2);
 // console.log(arr);
+
+// let arr = [1,2,3,4];
+// let newarray1 = arr.map((nums)=>nums*nums);
+// console.log(arr);
+// console.log(newarray1);
+ 
+// let arr1 = [10,3,53,54,65,3];
+// let newarray2 = arr1.filter((nums)=>nums<50);
+// console.log(newarray2);
+
+// let arr3 = [1,5,7,9];
+// let result = arr3.reduce((total,num)=>total+num,0);
+// console.log(result);
+
+// let arr3 = [1,5,7,9];
+// let result = arr3.reduce((total,num)=>total*num,1);
+// console.log(result);
+
+// destructring
+let arr = [2,4,6,8];
+const [x,y,z,w]=arr;
+console.log(x,y,z,w);
+
+let obj = {
+    name :"rishabh",
+    address :{
+        state :"up",
+        city : "gonda",
+        pin:271403
+    }
+}
+const{name,address:{state,city,pin}} = obj;
+console.log(name,state);
---
+++ ECMA6/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let arr3 = [1,5,7,9];
+// let result = arr3.reduce((total,num)=>total+num,0);
+// console.log(result);
