ede9dfcdc26c0e4580758e6e187fa31ae02ca579
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-07 15:16:44 +0530

    ECMA6 JS

---
+++ ECMA6/advance.js
@@ -0,0 +1,29 @@
+// const cube = (x,y,z)=>{return x*y*z};
+// console.log(cube(3,4,5));
+
+// function cube1(x){
+//     return x*x*x;
+// }
+// let name = "rishabh";
+// let age = 21;
+// console.log(`my name is ${name} and my age is ${age}`);
+
+// 
+// let arr = [1,2,3,4];
+// arr.push(5);
+// console.log(arr);
+// console.log(arr.length);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift(7);
+// console.log(arr);
+// let arr2 = [4,5,6];
+// let arr3 = arr.concat(arr2);
+// console.log(arr3);
+// let newarray1 = arr.slice(0,4)
+// console.log(newarray1);1,2,3,4
+// let newarray2 = arr.splice(0,3,100,200)
+// console.log(newarray2);
+// console.log(arr);
