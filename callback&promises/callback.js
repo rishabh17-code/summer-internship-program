e80d8a2111f665b823213e7ddc5e8d696d208a4b
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-08 15:50:18 +0530

    update callback&hell

--- callback&promises/callback.js
+++ callback&promises/callback.js
@@ -1,4 +1,6 @@
 // A callback is a function that is passed as an argument to another function and is executed later when a specific task is completed.
+
+
 // function greet(name, callback){
 //     console.log("hello "+name);
 //     callback();
@@ -25,10 +27,49 @@
 
 // An asynchronous callback is a callback function that does not execute immediately. Instead, it runs later, after an asynchronous task (such as a timer, file read, or API request) has completed.
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(()=> {
-    console.log("Inside Callback");
-}, 2000);
+// setTimeout(()=> {
+//     console.log("Inside Callback");
+// }, 2000);
 
-console.log("End");
+// console.log("End");
+ // callback hell
+//  setTimeout(()=>{
+//     console.log("task 1");
+//     setTimeout(()=>{
+//         console.log("task 2");
+        
+//     },5000);
+//  },3000);
+//  setTimeout(()=>{
+//             console.log("complete,task 4")
+//         },1000);
+
+// example of callback hell
+
+// function order(callback){
+//     setTimeout(()=>{ 
+//         console.log("order reciever");
+//         callback();
+//     },2000);
+// }
+// function prepare(callback){
+//     setTimeout(()=>{
+//         console.log("resaturent preparing food");
+//         callback();
+//     },2000);
+// }
+// function packed(callback){
+//     setTimeout(()=>{
+//         console.log("food packed");
+//         callback();
+//     },2000);
+// }
+// order(()=>{
+//  prepare(()=>{
+//     packed(() =>{
+//         console.log("order is ready for delivery");
+//     });
+//  });
+// });
