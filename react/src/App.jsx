b01f678413fbc63db01612dcb6c49d3d609fe27a
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-13 14:20:01 +0530

    initial react

---
+++ react/src/App.jsx
@@ -0,0 +1,10 @@
+import Student from "./Student";
+function App(){
+     let name = "rishabh";
+    return (
+   <div> <h1>hello {name} </h1>
+    <Student />
+    <Student /></div>
+    );
+}
+export default App;
---
+++ react/src/Student.jsx
@@ -0,0 +1,7 @@
+function Student() {
+    return (
+        <h2>I am Student Component</h2>
+    );
+}
+
+export default Student;
