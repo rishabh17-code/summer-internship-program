3857e1a1939dfa5eaead4b60237042a812d0b363
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-16 14:14:25 +0530

    usestate hook

--- react/src/hooks-state/usestate.jsx
+++ react/src/hooks-state/usestate.jsx
@@ -4,13 +4,30 @@ function Counter() {
   
   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <p>You clicked {count} times</p>
+  //     <button onClick={() => setCount(count + 1)}>
+  //       Click me
+  //     </button>
+  //   </div>
+  // );
+  return(
+        <>
+            <h2>{count}</h2>
+
+            <button onClick={() => setCount(count + 1)}>
+                increment
+            </button>
+
+            <button onClick={() => setCount(count - 1)}>
+                decrement
+            </button>
+
+            <button onClick={() => setCount(0)}>
+                Reset
+            </button>
+        </>
+    );
 }
 export default Counter;
