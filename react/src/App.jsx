bda6e117a7d170146054289c8042a17fdb9b37d6
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-16 12:02:29 +0530

    initial hooks

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,11 @@
 // import Student from "./component/Student";
+import Counter from "./hooks-state/usestate";
 function App(){
     //  let name = "rishabh";
     return (
+        <div>
+            <Counter/>
+        </div>
 //    <div> <h1>hello {name} </h1>
 //     <Student />
 //     <Student /></div>
