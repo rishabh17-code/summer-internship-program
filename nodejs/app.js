e35556c76e0bdc188840222a7c724b4e2502493a
rishabh17-code <rishabhsingh6422@gmail.com>
2026-07-21 14:51:52 +0530

    first server using node js and basic nodejs

---
+++ nodejs/app.js
@@ -0,0 +1,27 @@
+const add = require("./math");
+
+console.log(add(15, 5));
+
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//     res.end("Hello from Server");
+// });
+
+// server.listen(3000);
+
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+
+//     res.write("Hello from Node.js Server");
+
+//     res.end();
+
+// });
+
+// server.listen(5000);
+
+// console.log("Server running on port 5000");
